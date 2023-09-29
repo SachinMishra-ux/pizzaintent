b3e639ed5964f24b226e559ba29a8d31df04896d
Sachin <sachin.mishra@datasociety.com>
2023-09-30 00:38:46 +0530

    done3

--- main.py
+++ main.py
@@ -12,14 +12,23 @@ st.title('Intent Prediction App')
 
 # Create a text input widget for user input
 user_input = st.text_input('Enter a sentence:', '')
-
+# Define a mapping from numerical predictions to labels
+intent_mapping = {
+    0: 'payment',
+    1: 'order',
+    2: 'cancel/modify',
+    3: 'info'
+}
 # Create a button to trigger intent prediction
 if st.button('Predict Intent'):
     if user_input:
         # Make a prediction using the loaded model
         intent = intent_classifier.predict([user_input])[0]
+        # Map the numerical prediction to the corresponding label
+        intent_label = intent_mapping.get(intent, 'Unknown')
 
         # Display the predicted intent
         st.success(f'Predicted Intent: {intent}')
+        st.success(f'Predicted Intent: {intent_label}')
     else:
         st.warning('Please enter a sentence to predict the intent.')
