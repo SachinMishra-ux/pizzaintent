73e23941a3f10443f33894c5afa4fa72b80ce957
Sachin <sachin.mishra@datasociety.com>
2023-09-29 23:52:20 +0530

    done

---
+++ frontend/main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+intent_classifier = joblib.load("./intent_classifier.joblib")
+
+# Streamlit app title
+st.title('Intent Prediction App')
+
+# Create a text input widget for user input
+user_input = st.text_input('Enter a sentence:', '')
+
+# Create a button to trigger intent prediction
+if st.button('Predict Intent'):
+    if user_input:
+        # Make a prediction using the loaded model
+        intent = intent_classifier.predict([user_input])[0]
+
+        # Display the predicted intent
+        st.success(f'Predicted Intent: {intent}')
+    else:
+        st.warning('Please enter a sentence to predict the intent.')
